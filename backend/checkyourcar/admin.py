d91a3e11bbd4794044669a9f8b8d9e4b69158b9a
MelMony <31891015+MelMony@users.noreply.github.com>
2020-09-09 13:59:23 +1000

    AdminBackendWorking

--- backend/checkyourcar/admin.py
+++ backend/checkyourcar/admin.py
@@ -1,7 +1,24 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
+from .models import Car, Issue, User
 
-from .models import Car, Issue
 
-@admin.register(Car, Issue)
-class OwnerAdmin(admin.ModelAdmin):
-    pass
+
+class CarAdmin(ImportExportModelAdmin):
+    list_display = ['id', 'make', 'model', 'year']
+
+
+
+
+class UserAdmin(ImportExportModelAdmin):
+    list_display = ['id', 'name', 'email']
+
+
+
+class IssueAdmin(ImportExportModelAdmin):
+    list_display = ['id', 'name', 'recall', 'notification']
+
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Issue, IssueAdmin)
--- backend/checkyourcar/views.py
+++ backend/checkyourcar/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from tablib import Dataset
+from checkyourcar.resources import CarResources, IssueResources, UserResources
 
-# Create your views here.
+
+def simple_upload(request):
+    if request.method == 'POST':
+        car_resource = CarResources()
+        dataset = Dataset()
+        new_car = request.FILES['myfile']
+
+        imported_data = dataset.load(new_car.read())
+        result = car_resource.import_data(dataset, dry_run=True)  # Test the data import
+
+        if not result.has_errors():
+            car_resource.import_data(dataset, dry_run=False)  # Actually import now
+
+    return render(request, 'core/simple_upload.html')
