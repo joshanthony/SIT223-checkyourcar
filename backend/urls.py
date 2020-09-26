2cfed2f4e2de6165593ea0061377119f12de5d49
Josh Anthony <joshanthonymail@gmail.com>
2020-09-26 21:54:16 +0700

    update static files config

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from checkyourcar.views import CarSearch, CarList, IssueSearch, UserCarList, CarUpdateView, CarDestroyView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/issues/', IssueSearch.as_view()),
@@ -27,4 +28,5 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-autho/registration/', include('rest_auth.registration.urls')),
     path('api-auth', include('rest_framework.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
