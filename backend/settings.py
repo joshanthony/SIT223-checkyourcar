14e83d6e27bbab5b9a3197c86add69863024762b
Josh Anthony <joshanthonymail@gmail.com>
2020-09-26 23:54:48 +0700

    update static paths

--- backend/settings.py
+++ backend/settings.py
@@ -151,11 +151,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Place static in the same location as webpack build files
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
+STATICFILES_DIRS = []
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
