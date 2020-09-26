088cd6b5343e2ef2d7446baa22f28d28b74caf16
MelMony <31891015+MelMony@users.noreply.github.com>
2020-09-26 21:32:11 +1000

    b

--- backend/settings.py
+++ backend/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
